eeafc3f1af4e3be345dc89def2105a3a20fd0844
quangvuong2541 <truongquangvuong2000@gmail.com>
2023-01-11 09:26:50 +0700

    buoi1

---
+++ src/buoi1/buoi1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./buoi1.css"
+export default class Buoi1 extends Component {
+    render() {
+        return (
+            <div className='demo'>
+                <h1 > chào minh đức đẹp trai </h1>
+            </div>
+        )
+    }
+}
